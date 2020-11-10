19b8040bd1e5315d68bef313fb53c8d7414fcd17
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-10 23:54:26 +0000

    including links for shortened url

--- express_server.js
+++ express_server.js
@@ -1,29 +1,22 @@
 const express = require("express");
+
 const app = express();
 const PORT = 8080;
-const { generateRandomString } = require("./generateRandomString")
+const { generateRandomString } = require("./helperFunctions/generateRandomString");
+const { urlDatabase } = require("./url_database");
 
 app.set("view engine", "ejs");
-
-const urlDatabase= {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 //------Routing Handlers--------
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
 app.get('/urls', (req, res) => {
@@ -35,15 +28,23 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString(6); 
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL); 
 });
+
 
 
 app.listen(PORT, () => {
