095c3a0368794c1ef8ebaa50204720bfccc72a27
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-11 10:35:18 +0000

    including login/logout/cookies funcionalities

--- express_server.js
+++ express_server.js
@@ -1,30 +1,34 @@
 const express = require("express");
-
 const app = express();
 const PORT = 8080;
-const { generateRandomString } = require("./helperFunctions/generateRandomString");
 const { urlDatabase } = require("./url_database");
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
+const { generateRandomString } = require("./helperFunctions/generateRandomString");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-//------Routing Handlers--------
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+//------Routing Handlers--------//
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+};
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"] 
+  };
   res.render("urls_new");
 });
 
@@ -35,8 +39,21 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+  app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+  app.post("/logout", (req,res) => {
+    res.clearCookie("username");
+    res.redirect("/urls");
+  });
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
